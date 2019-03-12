2b23bbb36375bcf73cb794667c559d1d412ceb10
Wayne Collier <ke4tri@gmail.com>
2019-03-12 14:54:11 -0500

    first commit

--- PlanentsAndSpaceShips/Program.cs
+++ PlanentsAndSpaceShips/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PlanentsAndSpaceShips
 {
@@ -6,7 +7,26 @@ namespace PlanentsAndSpaceShips
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<string> planetList = new List<string>() { "Mercury", "Mars" };
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+            
+            List<string> lastPlanetList = new List<string>() { "Saturn", "Uranus" };
+            List<string> combinedList = new List<string>();
+
+            combinedList.AddRange(lastPlanetList);
+            combinedList.AddRange(planetList);
+
+
+            Console.WriteLine("This is from the combined list :");
+            foreach (var item in planetList)
+            {
+                //var listJoined = String.Join("", item);
+                //Console.Write(listJoined);
+                Console.Write(item + ", ");
+            }
+            Console.ReadLine();
+
         }
     }
 }
